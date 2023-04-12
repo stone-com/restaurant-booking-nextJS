2e1b090a19a23bc8871dce0768436102690d8aee
Stone Comstock <comstockstone@yahoo.com>
2023-04-12 21:10:34 +0700

    custom font sizes specified in tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <h1>Hello! This is going to be my new app!! </h1>
+    <main className="">
+      <h1 className='text-7xl'>Hello! This is going to be my new app!! </h1>
     </main>
   )
 }
