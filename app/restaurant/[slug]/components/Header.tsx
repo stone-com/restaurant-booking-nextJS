d382706612663f25765805774cfd2e09a9d8b01e
Stone Comstock <comstockstone@yahoo.com>
2023-05-21 21:01:45 +0700

    Dynamically display restaurant name in header, from slug

--- app/restaurant/[slug]/components/Header.tsx
+++ app/restaurant/[slug]/components/Header.tsx
@@ -1,9 +1,17 @@
-export default function Header() {
+export default function Header({ name }: { name: string }) {
+  // function to split slug into title
+  const renderTitle = () => {
+    const nameArray = name.split('-');
+    // get the last word of the name (the city) and wrap it in parenthesis for display purposes
+    nameArray[nameArray.length - 1] = `(${nameArray[nameArray.length - 1]})`;
+    return nameArray.join(' ')
+  };
+
   return (
     <div className='overflow-hidden h-96'>
       <div className='bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center'>
-        <h1 className='text-center text-white text-7xl captitalize text-shadow'>
-          Milestones Grill (Toronto)
+        <h1 className='text-center text-white capitalize text-7xl text-shadow'>
+          {renderTitle()}
         </h1>
       </div>
     </div>
--- app/restaurant/[slug]/layout.tsx
+++ app/restaurant/[slug]/layout.tsx
@@ -1,13 +1,15 @@
-import Header from "./components/Header";
+import Header from './components/Header';
 
 export default function RestaurantLayout({
   children,
+  params
 }: {
   children: React.ReactNode;
+  params: { slug: string };
 }) {
   return (
     <>
-      <Header />
+      <Header name={params.slug}/>
       <div className='flex items-start justify-between w-2/3 m-auto 0 -mt-11'>
         {children}
       </div>
