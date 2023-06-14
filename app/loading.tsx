2bd2d211550cbf49d1d32f01430d00a81ab2f03c
Stone Comstock <comstockstone@yahoo.com>
2023-06-14 14:16:22 +0700

    Added loading page for restaurant page

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,6 +5,7 @@ export default function Loading() {
     <main>
       <Header />
       <div className='flex flex-wrap justify-center py-3 mt-10 px-36'>
+        {/* create 12 blank/grey cards to display when loading */}
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((num) => (
           <div
             className='w-64 m-3 overflow-hidden border rounded cursor-pointer animate-pulse bg-slate-200 h-72'
