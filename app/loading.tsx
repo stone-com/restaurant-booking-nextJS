eebff539b62d9a7363c6324060fab001e0bf67f3
Stone Comstock <comstockstone@yahoo.com>
2023-06-14 14:11:46 +0700

    added loading state with fake cards

---
+++ app/loading.tsx
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+
+export default function Loading() {
+  return (
+    <main>
+      <Header />
+      <div className='flex flex-wrap justify-center py-3 mt-10 px-36'>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((num) => (
+          <div
+            className='w-64 m-3 overflow-hidden border rounded cursor-pointer animate-pulse bg-slate-200 h-72'
+            key={num}
+          ></div>
+        ))}
+      </div>
+    </main>
+  );
+}
